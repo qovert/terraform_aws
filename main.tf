494b5e14b04fdc977fe39c6f7ccb89ccb344b578
Brian H <brian@qov.io>
2018-11-08 13:17:37 -0800

    Terraform AWS testing/examples again

--- main.tf
+++ main.tf
@@ -5,8 +5,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami		= "ami-079b4e9085609225c"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+	      #!/bin/bash
+	      echo "It lives!" > index.html
+	      nohup busybox httpd -f -p 8080 &
+	      EOF
 
   tags {
     Name = "test_machine"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-testing-instance"
+  
+  ingress {
+    from_port	= 8080
+    to_port	= 8080
+    protocol	= "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
